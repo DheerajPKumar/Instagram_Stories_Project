648dcf95643c64d4c09a7f41f5bf6fc0110748c9
DheerajPKumar <dheeraj.kumar@axeno.co>
2024-09-03 12:52:54 +0530

    updated the storyplayer.

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const stories = [
     { id: 'story4', videoSrc: 'https://cdn.pixabay.com/video/2015/09/04/685-138357242_tiny.mp4', imgSrc: 'https://plus.unsplash.com/premium_photo-1661274118914-248c2e1b6d2c?q=80&w=1997&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
     { id: 'story5', videoSrc: 'https://cdn.pixabay.com/video/2023/05/14/163058-827112818_tiny.mp4', imgSrc: 'https://images.unsplash.com/photo-1668425196248-8736f79130fc?q=80&w=1881&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
     { id: 'story6', videoSrc: 'https://cdn.pixabay.com/video/2023/01/13/146336-789093861_tiny.mp4', imgSrc: 'https://images.unsplash.com/photo-1658310868281-b311aa001197?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-    { id: 'story7', videoSrc: 'https://cdn.pixabay.com/video/2020/08/14/47213-451041047_tiny.mp4', imgSrc: 'https://plus.unsplash.com/premium_photo-1674929041956-f9fbbbdcc227?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { id: 'story7', videoSrc: 'https://cdn.pixabay.com/video/2024/01/23/197898-905833761_tiny.mp4', imgSrc: 'https://plus.unsplash.com/premium_photo-1674929041956-f9fbbbdcc227?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
     { id: 'story8', videoSrc: 'https://cdn.pixabay.com/video/2024/06/21/217668_tiny.mp4', imgSrc: 'https://images.unsplash.com/photo-1578588979923-639f39647d71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
     { id: 'story9', videoSrc: 'https://cdn.pixabay.com/video/2023/07/24/173031-848232067_tiny.mp4', imgSrc: 'https://images.unsplash.com/photo-1609660100545-05f3799a941b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
     { id: 'story10', videoSrc: 'https://cdn.pixabay.com/video/2023/12/10/192743-893446849_tiny.mp4', imgSrc: 'https://images.unsplash.com/photo-1502613374390-8da7aa532177?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
@@ -62,18 +62,30 @@ function update() {
     
     if (currentIndex > 0) {
         let prevStory = stories[currentIndex - 1];
-        const prevImage = document.createElement('img');
-        prevImage.src = stories[currentIndex-1].imgSrc;
-        prevImage.onclick = () => storyPlay(prevStory.id);
-        storyPrev.appendChild(prevImage);
+        // const prevImage = document.createElement('img');
+        // prevImage.src = stories[currentIndex-1].imgSrc;
+        // prevImage.onclick = () => storyPlay(prevStory.id);
+        // storyPrev.appendChild(prevImage);
+        const prevVideo = document.createElement('video');
+        prevVideo.src = prevStory.videoSrc;
+        prevVideo.classList.add('storyplayer__content-pVideo');
+        prevVideo.controls = true;
+        prevVideo.onclick = () => storyPlay(prevStory.id);
+        storyPrev.appendChild(prevVideo);
     }
 
     if (currentIndex < stories.length - 1) {
         const nextStory = stories[currentIndex + 1];
-        const nextImage = document.createElement('img');
-        nextImage.src = stories[currentIndex+1].imgSrc;
-        nextImage.onclick = () => storyPlay(nextStory.id);
-        storyNext.appendChild(nextImage);
+        // const nextImage = document.createElement('img');
+        // nextImage.src = stories[currentIndex+1].imgSrc;
+        // nextImage.onclick = () => storyPlay(nextStory.id);
+        // storyNext.appendChild(nextImage);
+        const nextVideo = document.createElement('video');
+        nextVideo.src = nextStory.videoSrc;
+        nextVideo.classList.add('storyplayer__content-nVideo');
+        nextVideo.controls = true;
+        nextVideo.onclick = () => storyPlay(nextStory.id);
+        storyNext.appendChild(nextVideo);
     }
 }
 
